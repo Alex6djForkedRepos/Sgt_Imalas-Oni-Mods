46ff1e7dba936826160687d231f2387e6699f674
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-07-03 19:52:24 +0200

    CritterTraits: change patching target to new method

--- CritterTraitsReborn/Source/Patches/EntityTemplates.cs
+++ CritterTraitsReborn/Source/Patches/EntityTemplates.cs
@@ -2,34 +2,12 @@
 using System;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using static EntityTemplates;
 using static FactionManager;
 
 namespace CritterTraitsReborn.Patches
 {
-	[HarmonyPatch(typeof(EntityTemplates), nameof(EntityTemplates.ExtendEntityToBasicCreature),
-		[
-		typeof(bool),
-		typeof(GameObject),
-		typeof(string),
-		typeof(string),
-		typeof(string),
-		typeof(FactionManager.FactionID),
-		typeof(string),
-		typeof(string),
-		typeof(NavType),
-		typeof(int),
-		typeof(float),
-		typeof(string),
-		typeof(float),
-		typeof(bool),
-		typeof(bool),
-		typeof(float),
-		typeof(float),
-		typeof(float),
-		typeof(float)
-		])]
-
-
+	[HarmonyPatch(typeof(EntityTemplates), nameof(EntityTemplates.ExtendEntityToBasicCreature), new Type[] { typeof(EntityTemplates.ExtendEntityToBasicCreatureData) })]
 	class EntityTemplates_ExtendEntityToBasicCreature
 	{
 		static void Postfix(ref GameObject __result)
