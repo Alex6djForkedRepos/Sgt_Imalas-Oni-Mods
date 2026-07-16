549461b2a24de28be8e3f1f8afed8a74d75e56d0
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-07-16 15:38:23 +0200

    NaturalConstruction: text filter

---
+++ NaturalConstruction/Content/Scripts/MaterialFilterBar.cs
@@ -0,0 +1,74 @@
+﻿using STRINGS;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using TMPro;
+using static UnityEngine.GraphicsBuffer;
+
+namespace NaturalConstruction.Content.Scripts
+{
+	internal class MaterialFilterBar : KMonoBehaviour
+	{
+		KButton ClearButton;
+		TMP_InputField TextInput;
+		MaterialSelector Target;
+
+		public void Init(MaterialSelector instance)
+		{
+			Target = instance;
+			var button = transform.Find("ClearButton").gameObject;
+			ClearButton = button.GetComponent<KButton>();
+			TextInput = transform.Find("LocTextInputField").gameObject.GetComponent<TMP_InputField>();
+
+			if (button.TryGetComponent<ToolTip>(out var tt))
+				tt.SetSimpleTooltip(global::STRINGS.UI.BUILDMENU.CLEAR_SEARCH_TOOLTIP);
+
+			TextInput.onFocus = TextInput.onFocus + (()=>OnStartEdit());
+			TextInput.onEndEdit.AddListener(OnEndEdit);
+
+			ClearButton.ClearOnClick();
+			ClearButton.onClick += () => TextInput.text = string.Empty;
+		}
+		void OnEndEdit(object _) => Target.isEditing = false;
+		void OnStartEdit() => Target.isEditing = true;
+
+		public void Clear()
+		{
+			TextInput.text = string.Empty;
+		}
+		public bool HasFilter() => TextInput.text.Any();
+
+		static readonly Dictionary<Tag, string> CachedTagNames = [];
+
+		public bool IsTagWithinFilters(Tag tag)
+		{
+			if (!HasFilter())
+				return true;
+
+			string filter = TextInput.text;
+			if(StringInFilter(tag.ToString(),filter))
+				return true;
+
+			if(!CachedTagNames.TryGetValue(tag, out var name))
+			{
+				var prefab = Assets.TryGetPrefab(tag);
+				if(prefab != null)
+					CachedTagNames[tag] = name = UI.StripLinkFormatting(prefab.GetProperName());
+				else
+					CachedTagNames[tag] = name = tag.ToString();
+			}
+			return StringInFilter(name, filter);
+		}
+
+		bool StringInFilter(string text, string filter)
+		{
+			return CultureInfo.InvariantCulture.CompareInfo.IndexOf(
+										text,
+										filter,
+										CompareOptions.IgnoreCase
+									) >= 0;
+		}
+	}
+}
---
+++ NaturalConstruction/Patches/MaterialSelector_Patches.cs
@@ -0,0 +1,90 @@
+﻿using HarmonyLib;
+using NaturalConstruction.Content.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using UnityEngine;
+using UtilLibs;
+
+namespace NaturalConstruction.Patches
+{
+	internal class MaterialSelector_Patches
+	{
+        static ConditionalWeakTable<MaterialSelector, MaterialFilterBar> SearchBars = [];
+
+        /// <summary>
+        /// happens after all the toggles have been created and put into the correct order
+        /// </summary>
+        [HarmonyPatch(typeof(MaterialSelector), nameof(MaterialSelector.UpdateScrollBar))]
+        public class MaterialSelector_UpdateScrollBar_Patch
+        {
+            public static void Prefix(MaterialSelector __instance)
+			{
+				if (!SearchBars.TryGetValue(__instance, out MaterialFilterBar searchbar))
+					return;
+
+				if (!searchbar.HasFilter())
+					return;
+
+
+				foreach (KeyValuePair<Tag, KToggle> elementToggle in __instance.ElementToggles)
+				{
+					if (elementToggle.Value.gameObject.activeSelf && !searchbar.IsTagWithinFilters(elementToggle.Key))
+					{
+						elementToggle.Value.gameObject.SetActive(false);
+					}
+				}
+			}
+        }
+
+        [HarmonyPatch(typeof(MaterialSelector), nameof(MaterialSelector.ConfigureScreen))]
+        public class MaterialSelector_ConfigureScreen_Patch
+        {
+            public static void Postfix(MaterialSelector __instance)
+            {
+                bool isSearchableTag = __instance.ElementToggles.Count > 20;
+
+				if (!SearchBars.TryGetValue(__instance, out var searchbar))
+				{
+					searchbar = AddSearchBarToSelector(__instance);
+					SearchBars.Add(__instance, searchbar);
+				}
+				searchbar.Clear();
+				searchbar.gameObject.SetActive(isSearchableTag);
+
+			}
+
+			private static MaterialFilterBar AddSearchBarToSelector(MaterialSelector instance)
+			{
+                if(SearchBarPrefab == null)
+				{
+					var prefabGo = ScreenPrefabs.Instance.RetiredColonyInfoScreen.gameObject;
+                    if(prefabGo == null)
+                    {
+                        SgtLogger.error("RetiredColonyInfoScreen was null!");
+                        return null;
+					}
+					var searchBar = prefabGo.transform.Find("Content/ColonyData/Colonies and Achievements/Colonies/Search");
+					if (searchBar == null)
+					{
+						SgtLogger.error("RetiredColonyInfoScreen.Searchbar was not found!");
+						return null;
+					}
+                    SearchBarPrefab = Util.KInstantiateUI(searchBar.gameObject);
+				}
+
+                var searchbarGO = Util.KInstantiateUI(SearchBarPrefab, instance.gameObject, true);
+                var search = searchbarGO.AddOrGet<MaterialFilterBar>();
+                search.Init(instance);
+
+
+				searchbarGO.transform.SetSiblingIndex(instance.transform.Find("Container").transform.GetSiblingIndex());
+
+				return search;
+
+			}
+		}
+        static GameObject SearchBarPrefab = null;
+	}
+}
