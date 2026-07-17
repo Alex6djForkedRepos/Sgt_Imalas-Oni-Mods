c287299a9498a7a85e3194f5e839fd8fd015f634
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-07-17 09:44:12 +0200

    RE: vanilla cargo bay insulate critters

---
+++ Rockets-TinyYetBig/Patches/CritterTemperatureMonitor_Patches.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Rockets_TinyYetBig.Patches
+{
+	internal class CritterTemperatureMonitor_Patches
+	{
+
+        [HarmonyPatch(typeof(CritterTemperatureMonitor.Instance), nameof(CritterTemperatureMonitor.Instance.IsEntirelyInVaccum))]
+        public class CritterTemperatureMonitor_Instance_IsEntirelyInVaccum_Patch
+		{
+			/// <summary>
+			/// If the critter is in a cargo bay, it should always fall back to its internal temperature
+			/// </summary>
+			/// <param name="__instance"></param>
+			/// <param name="__result"></param>
+			[HarmonyPriority(Priority.LowerThanNormal)]
+			public static void Postfix(CritterTemperatureMonitor.Instance __instance, ref bool __result)
+			{
+				if (__instance.gameObject.HasTag(GameTags.Creatures.TrappedInCargoBay))
+					__result = true;
+			}
+		}
+
+		[HarmonyPatch(typeof(CritterTemperatureMonitor.Instance), nameof(CritterTemperatureMonitor.Instance.GetTemperatureExternal))]
+		public static class CritterTemperatureMonitor_GetTemperatureExternal_Patch
+		{
+			public static bool Prefix(CritterTemperatureMonitor.Instance __instance, ref float __result)
+			{
+				if (!__instance.gameObject.HasTag(GameTags.Creatures.TrappedInCargoBay))
+					return true;
+
+				__result = __instance.GetTemperatureInternal();
+				return false;
+			}
+		}
+	}
+}
