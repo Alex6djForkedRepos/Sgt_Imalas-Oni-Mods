c6cd8efd067175a2127333df175900677a28b028
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-07-23 13:36:52 +0200

    aquaticminnow: fix missing consumablesconsumer defaults crashing consumable screen

---
+++ AquaticMinnowMinion/Patches/ConsumableConsumer_Patches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaticMinnowMinion.Patches
+{
+	internal class ConsumableConsumer_Patches
+	{
+
+        [HarmonyPatch(typeof(ConsumableConsumer), nameof(ConsumableConsumer.SetModelDietaryRestrictions))]
+        public class ConsumableConsumer_SetModelDietaryRestrictions_Patch
+        {
+            public static void Postfix(ConsumableConsumer __instance)
+			{
+				if (__instance.HasTag(ModAssets.Tags.AquaticMinion))
+				{
+					__instance.dietaryRestrictionTagSet = [.. ConsumerManager.instance.StandardDuplicantDietaryRestrictions];
+				}
+			}
+        }
+	}
+}
